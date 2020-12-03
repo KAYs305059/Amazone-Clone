26fb7ca3f24192d5811979d0e957d8d39e82545d
Khadar Ahmed Yusuf <Khadar_yo@hotmail.com>
2020-12-03 06:45:54 +0100

    done with header comp

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
   return (
     <nav className='header'>
-      {/* Logo on the left */}
-      <Link to='/login'>
+      <Link to='/'>
         <img
           className='header__logo'
           src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
@@ -15,40 +15,40 @@ function Header() {
         />
       </Link>
 
-      {/* Search box */}
       <div className='header__search'>
         <input type='text' className='header__searchInput' />
         <SearchIcon className='header__searchIcon' />
       </div>
 
-      {/* 3 links  */}
       <div className='header__nav'>
-        {/* Link 1 */}
         <Link to='/login' className='header__link'>
           <div className='header__option'>
-            <span>Hello Khadar</span>
-            <span>Sign in</span>
+            <span className='header__optionLineOne'>Hello Khadar</span>
+            <span className='header__optionLineTwo'>Sign in</span>
           </div>
         </Link>
 
-        {/* Link 2 */}
         <Link to='/' className='header__link'>
           <div className='header__option'>
-            <span>Returns</span>
-            <span>& Orders</span>
+            <span className='header__optionLineOne'>Returns</span>
+            <span className='header__optionLineTwo'>& Orders</span>
           </div>
         </Link>
 
-        {/* Link 3 */}
         <Link to='/' className='header__link'>
           <div className='header__option'>
-            <span>Your</span>
-            <span>Prime</span>
+            <span className='header__optionLineOne'>Your</span>
+            <span className='header__optionLineTwo'>Prime</span>
+          </div>
+        </Link>
+
+        <Link to='/checkout' className='header__link'>
+          <div className='header__optionBasket'>
+            <ShoppingBasketIcon />
+            <span className='header__optionLineTwo header__basketCount'>0</span>
           </div>
         </Link>
       </div>
-
-      {/* Basket icon with a number */}
     </nav>
   );
 }
